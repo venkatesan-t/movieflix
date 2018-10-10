fe79b4501a0a59c012e215c1c24f0a914597e166
Venkatesan Thirumalaisamy <t.venk8@gmail.com>
2018-10-10 10:24:32 +0530

    added Spring Boot Web API

---
+++ movieflixwebapi/src/main/java/com/venkat/movieflixwebapi/MovieflixWebapiApplication.java
@@ -0,0 +1,12 @@
+package com.venkat.movieflixwebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieflixWebapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieflixWebapiApplication.class, args);
+    }
+}
