f2e311df21c71c256d9fd96ea42e4ddc2e2db43d
Venkatesan Thirumalaisamy <t.venk8@gmail.com>
2018-10-11 09:31:08 +0530

    added sayHello and addMovieRecommendation methods in service added MovieRecommendationDTO

--- movieflixwebapi/src/main/java/com/venkat/movieflixwebapi/MovieflixWebapiApplication.java
+++ movieflixwebapi/src/main/java/com/venkat/movieflixwebapi/MovieflixWebapiApplication.java
@@ -1,12 +1,31 @@
 package com.venkat.movieflixwebapi;
 
+import com.venkat.movieflixwebapi.dto.MovieRecommendationDTO;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class MovieflixWebapiApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         SpringApplication.run(MovieflixWebapiApplication.class, args);
+    }
+
+    @RequestMapping("hello")
+    public String sayHello()
+    {
+        return "Hello from Spring Boot Application...";
+    }
+
+    @RequestMapping(value = "/movies", method = RequestMethod.POST,
+                    consumes = "application/json", produces = "application/json")
+    public void addMovieRecommendation(@RequestBody MovieRecommendationDTO movieRecommendation)
+    {
     }
 }
---
+++ movieflixwebapi/src/main/java/com/venkat/movieflixwebapi/dto/MovieRecommendationDTO.java
@@ -0,0 +1,28 @@
+package com.venkat.movieflixwebapi.dto;
+
+public class MovieRecommendationDTO {
+    public String title;
+    public String alsoViewedTitle;
+
+    public MovieRecommendationDTO()
+    {
+        this.title = "Dark Knight";
+        this.alsoViewedTitle = "Memento";
+    }
+
+    /*public MovieRecommendationDTO(String title, String alsoViewedTitle)
+    {
+        this.title = title;
+        this.alsoViewedTitle = alsoViewedTitle;
+    }*/
+
+    public String getTitle()
+    {
+        return this.title;
+    }
+
+    public String getAlsoViewedTitle()
+    {
+        return this.alsoViewedTitle;
+    }
+}
